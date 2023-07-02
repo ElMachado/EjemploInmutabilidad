d5249e3a9bacbd3e2e1907f02dfa33ff469fb6e7
Jesús D Machado <jdarroyo@sura.com.co>
2023-07-02 10:27:25 -0500

    Initial commit

---
+++ src/ImmutableStudent.java
@@ -0,0 +1,33 @@
+public final class ImmutableStudent {
+    private final int id;
+    private final String name;
+    private final Age age;
+
+    public ImmutableStudent(int id, String name, Age age) {
+       Age cloneAge = new Age();
+        cloneAge.setDay(age.getDay());
+        cloneAge.setMonth(age.getMonth());
+        cloneAge.setYear(age.getYear());
+        this.age = cloneAge;
+        this.name = name;
+        this.id = id;
+        //this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Age getAge() {
+        Age cloneAge = new Age();
+        cloneAge.setDay(this.age.getDay());
+        cloneAge.setMonth(this.age.getMonth());
+        cloneAge.setYear(this.age.getYear());
+        return cloneAge;
+       // return age;
+    }
+}
